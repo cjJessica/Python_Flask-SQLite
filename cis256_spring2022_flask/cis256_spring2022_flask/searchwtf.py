c38f2c5bae7229a2e201408e22548257f724df43
Charli <cjpapetoo@gmail.com>
2025-11-20 15:32:05 -0700

    Changed publisher part in searchwtf.py

--- cis256_spring2022_flask/cis256_spring2022_flask/searchwtf.py
+++ cis256_spring2022_flask/cis256_spring2022_flask/searchwtf.py
@@ -15,7 +15,9 @@ class ByAuthorIdWTF(FlaskForm):
 
 
 class ByPublisherIdWTF(FlaskForm):
-    pass
+    mydb = BooksDB()
+    publishers = mydb.getpublishers()
+    publisher_choice = SelectField("PublisherChoice", choices=publishers)
 
 class ByTitleWTF(FlaskForm):
     # This will also need a stringfield for the words the user types
